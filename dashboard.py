c660603be7458a66f0076491b0c7042d933d58c2
Marina Frei <marina.frei@stud.fhgr.ch>
2025-06-17 20:52:46 +0200

    Balkendiagramm hinzugefügt

--- dashboard.py
+++ dashboard.py
@@ -12,6 +12,7 @@ with open("./Kartendaten/transformiert/Kartendaten_Gemeinden.geojson") as f:
 df_results = pd.read_excel("results.xlsx")
 df_gemeinden = pd.read_excel("Gemeinden_Graubünden.xlsx")
 df_spitaeler = pd.read_excel("Spitalliste_bereinigt_fuer_Dashboard.xlsx")
+df_population = pd.read_excel("Bevölkerung_Kanton_Graubünden.xlsx", usecols="A,AA,AB,AC", header=1)
 
 spitaeler_namelist_all = df_spitaeler["Spitalname"].values.tolist()
 spitaeler_namelist_current = spitaeler_namelist_all[:7]
@@ -29,7 +30,7 @@ app.layout = html.Div([
     ]),
     dbc.Row([
         dbc.Col([dcc.Graph(id="graph-map")], width=8),
-        dbc.Col([dcc.Graph(id="graph-hist")], width=4)
+        dbc.Col([dcc.Graph(id="graph-bar")], width=4)
     ], style={"margin-top":"1rem"})
     ], style={"margin":".5rem 1rem"}) #Endtag html.DIV
 
@@ -42,19 +43,21 @@ def reset_dropdown(n_clicks):
     return spitaeler_namelist_current
 
 
-@callback(Output("graph-map", "figure"),
+@callback([Output("graph-map", "figure"),
+          Output("graph-bar", "figure")],
           Input("spitaeler_dropdown", "value"))
 
-def create_df_for_map(gewaehlte_spitaeler):
+def create_figures(gewaehlte_spitaeler):
     df_results_filtered = df_results[df_results["Spitalname"].isin(gewaehlte_spitaeler)]
     min_fahrzeiten = df_results_filtered.groupby("GDENAME")["Fahrzeit in min"].min().reset_index()
     min_fahrzeiten.columns = ["GDENAME", "MinFahrzeit"]
 
     df_gemeinden_subset = df_gemeinden[["GDEHISTID", "GDENAME"]]
     df_min_fahrzeit = df_gemeinden_subset.merge(min_fahrzeiten, on="GDENAME", how="inner")
+    df_population_subset = df_population[["GDENAME", "Alle betroffenen Personen"]]
+    df_min_fahrzeit = df_min_fahrzeit.merge(df_population_subset, on="GDENAME", how="inner")
 
-
-    fig = px.choropleth(
+    fig_map = px.choropleth(
         df_min_fahrzeit,
         geojson=gemeinden,
         locations="GDEHISTID",
@@ -66,9 +69,25 @@ def create_df_for_map(gewaehlte_spitaeler):
         title="Minimale Fahrzeit zur Geburtshilfe pro Gemeinde"
     )
 
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
-    return fig
+    fig_map.update_geos(fitbounds="locations", visible=False)
+    fig_map.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+
+    bins = list(range(0, 120, 10)) + [120, 260]
+    labels = ['0-9 Min', '10-19 Min', '20-29 Min', '30-39 Min', '40-49 Min', '50-59 Min', '60-69 Min', '70-79 Min', '80-89 Min', '90-99 Min', '100-109 Min', '110-119 Min', '120+ Min']
+    df_min_fahrzeit["FahrzeitKlasse"] = pd.cut(df_min_fahrzeit["MinFahrzeit"], bins=bins, labels=labels, right=False)
+    df_min_fahrzeit = df_min_fahrzeit.sort_values(by="FahrzeitKlasse")
+    df_grouped = df_min_fahrzeit.groupby("FahrzeitKlasse", as_index=False, observed=False)["Alle betroffenen Personen"].sum()
+        
+    fig_bar = px.bar(
+        df_grouped,
+        x="FahrzeitKlasse",
+        y="Alle betroffenen Personen",
+        title="Fahrzeitverteilung"
+    )
+
+    fig_bar.update_layout(xaxis_title="Fahrzeit in Minuten", yaxis_title="Anzahl betroffene Personen")
+
+    return fig_map, fig_bar
 
 if __name__ == '__main__':
     app.run(debug=True)
